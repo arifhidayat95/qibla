e8f80d4620f22ca431c407c6abd5f958c7d042a8
rizentium <rizentium@gmail.com>
2020-04-18 18:03:58 +0700

    Added magnetometer on project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Magnetometer, MagnetometerReading } from '@ionic-native/magnetometer/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  data: MagnetometerReading;
 
-  constructor() {}
+  constructor(private magnetometer: Magnetometer) {
+    this.magnetometer.watchReadings().subscribe(res => {
+      this.data = res;
+    })
+  }
 
 }
